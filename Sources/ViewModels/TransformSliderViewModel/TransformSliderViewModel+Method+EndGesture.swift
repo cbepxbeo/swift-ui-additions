de04bbf210889c04fc2f93958687a2a2e1469b8a
cbepxbeo <cbepxbeo@mail.ru>
2023-05-20 16:18:20 +0300

    add TransformSliderViewModel+Method+EndGesture

---
+++ Sources/ViewModels/TransformSliderViewModel/TransformSliderViewModel+Method+EndGesture.swift
@@ -0,0 +1,29 @@
+/*
+
+Project: SwiftUIAdditions
+File: TransformSliderViewModel+Method+EndGesture.swift
+Created by: Егор Бойко
+Date: 20.05.2023
+
+Status: #In progress | #Not decorated
+
+*/
+
+import SwiftUI
+
+extension TransformSliderViewModel {
+    internal func endGesture(value: DragGesture.Value, count: Int){
+        let currentOffset = value.translation.width
+        let count: CGFloat = .init(count - 1)
+        
+        if currentOffset < -self.swipeDistance && self.current != -count {
+            current -= 1
+
+        } else if currentOffset > self.swipeDistance && self.current != 0 {
+            self.current += 1
+        }
+        withAnimation(.spring()) {
+            self.offset = self.total
+        }
+    }
+}
