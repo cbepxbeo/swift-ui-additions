9fd0f56114f3fee9fd5af6793ff9aad27fde2c67
cbepxbeo <cbepxbeo@mail.ru>
2023-05-21 18:26:18 +0300

    OffsetContentStyle

---
+++ Sources/Protocols/OffsetContentStyle/OffsetContentStyle.swift
@@ -0,0 +1,26 @@
+/*
+
+Project: SwiftUIAdditions
+File: OffsetContent.swift
+Created by: Егор Бойко
+Date: 21.05.2023
+
+Status: #Completed | #Not decorated
+
+*/
+
+import SwiftUI
+
+public protocol OffsetContentStyle {
+    var blurEffect: CGFloat? { get set }
+    var offsetContent: CGFloat? { get set }
+    var automaticOffset: (offset: CGFloat, value: Bool) { get set }
+    var scaleEffect: CGFloat? { get set }
+    var beforeShowBackgroundColor: Color? { get set }
+    var afterShowBackgroundColor: Color? { get set }
+    var overlayColor: Color? { get set }
+    var opacityOverlayColor: CGFloat? { get set }
+    var animationBackgroundColor: Animation? { get set }
+    var hideAnimation: Animation? { get set }
+    var showAnimation: Animation? { get set }
+}
