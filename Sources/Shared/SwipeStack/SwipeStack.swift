c3b68722c25bf136976335402f1e6cbe5535e8d0
cbepxbeo <cbepxbeo@mail.ru>
2023-11-05 17:29:36 +0300

    Update SwipeStack.swift

--- Sources/Shared/SwipeStack/SwipeStack.swift
+++ Sources/Shared/SwipeStack/SwipeStack.swift
@@ -10,3 +10,59 @@
  */
 
 import SwiftUI
+import SwiftUIExtension
+
+public struct SwipeStack<Content: View>: View  {
+    public init(distance: CGFloat = 50, @ViewBuilder content: @escaping () -> Content) {
+        self.distance = distance
+        self.content = content
+    }
+    
+    @State private var drag: SwipeModifier.Direction? = nil
+    var swipeLeftAction: (() -> ())? = nil
+    var swipeRightAction: (() -> ())? = nil
+    var swipeDownAction: (() -> ())? = nil
+    var swipeUpAction: (() -> ())? = nil
+    let distance: CGFloat
+    let content: () -> Content
+    
+    public func swipeAction(_ direction: SwipeModifier.Direction, action: @escaping () -> ()) -> Self {
+        var output = self
+        switch direction {
+        case .left:
+            output.swipeLeftAction = action
+        case .right:
+            output.swipeRightAction = action
+        case .down:
+            output.swipeDownAction = action
+        case .up:
+            output.swipeUpAction = action
+        }
+        return output
+    }
+    
+    public var body: some View {
+        self.content()
+            .modifier(
+                SwipeModifier(
+                    drag: self.$drag,
+                    distance: self.distance
+                )
+            )
+            .onChange(of: drag) { output in
+                switch output {
+                case .left:
+                    self.swipeLeftAction?()
+                case .right:
+                    self.swipeRightAction?()
+                case .down:
+                    self.swipeDownAction?()
+                case .up:
+                    self.swipeUpAction?()
+                default:
+                    break
+                }
+            }
+    }
+}
+
